23f4aa8019ea5097e223d0b7096bdb4cbedec76c
Martin Nyberg Hansen <martin.hansen@lego.com>
2024-03-01 15:15:06 +0100

    fix: changed setup to not use venv

--- monitoring/camera-pi/setup.sh
+++ monitoring/camera-pi/setup.sh
@@ -6,14 +6,5 @@ sudo apt-get update
 # Install system dependencies
 sudo apt-get install -y libraspberrypi0 libraspberrypi-bin libraspberrypi-dev
 
-# Create a virtual environment
-python3 -m venv env
-
-# Activate the virtual environment
-source env/bin/activate
-
 # Install Python dependencies
 pip install -r requirements.txt
-
-# Deactivate the virtual environment
-deactivate
