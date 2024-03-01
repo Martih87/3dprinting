048d3326e2b02bcd86cbd26ddce27c9f741e2bde
Martin Nyberg Hansen <martin.hansen@lego.com>
2024-03-01 15:54:51 +0100

    fixing...

--- monitoring/camera-pi/setup.sh
+++ monitoring/camera-pi/setup.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 # Install system dependencies
 sudo apt-get install -y libraspberrypi0 libraspberrypi-bin libraspberrypi-dev
 # pip for python packages
-sudo apt-get install pip
+sudo apt-get install -y pip git vim
 
 
 # Install Python dependencies
