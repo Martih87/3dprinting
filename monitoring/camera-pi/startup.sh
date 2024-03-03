06aba9748994dbe74e6f65a033ddafd7ec506074
Martin Nyberg Hansen <martin.hansen@lego.com>
2024-03-03 20:45:20 +0100

    startup script etc

--- monitoring/camera-pi/startup.sh
+++ monitoring/camera-pi/startup.sh
@@ -3,7 +3,8 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd ~projects/3dprinting/monitoring/camera-pi/src
+cd ~/projects/3dprinting/monitoring/camera-pi/src
+sudo pip install -r requirements.txt
 sudo python cleanup.py
 sudo python app.py
 cd /
