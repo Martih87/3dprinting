4434387f8bb003ca45109cf3e80e830ea1e77077
Martin Nyberg Hansen <martin.hansen@lego.com>
2024-03-03 20:39:17 +0100

    added screen to setup.sh

--- monitoring/camera-pi/setup.sh
+++ monitoring/camera-pi/setup.sh
@@ -4,7 +4,7 @@
 sudo apt-get update
 
 # Install system dependencies
-sudo apt-get install -y libraspberrypi0 libraspberrypi-bin libraspberrypi-dev
+sudo apt-get install -y libraspberrypi0 libraspberrypi-bin libraspberrypi-dev screen
 # pip for python packages
 sudo apt-get install -y pip git vim
 
